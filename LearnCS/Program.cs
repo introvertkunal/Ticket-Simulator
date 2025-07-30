73521e78ff74a9ef7e7a8a7b933b0f9769748e36
introvertkunal <kunal.singh@wonderbiz.in>
2025-07-30 18:04:51 +0530

    some changes

--- LearnCS/Program.cs
+++ LearnCS/Program.cs
@@ -7,37 +7,16 @@ using System.Text;
 namespace LearnCS
 {
 
-    class Window
+    public class Window
     {
         public Queue<int> Line=new Queue<int>();
     }
     public class TicketCounter
     {
-        public static void Main(String[] args)
-
+        public static void Simualte(int numberPeople,List<Window> windows)
         {
-            
-
-            Window w1 = new Window();
-            Window w2 = new Window();
-            Window w3 = new Window();
-            Window w4 = new Window();
-
-            //int[] counter = new int[4];
-
-            List<Window> windows = new List<Window>();
-            windows.Add(w1);
-            windows.Add(w2);
-            windows.Add(w3);
-            windows.Add(w4);
-
-
-            Console.WriteLine("Enter the number of people:");
-            long numberOfPeople = long.Parse(Console.ReadLine());
-
-
             int minIndex = 0;
-            while (numberOfPeople != 0)
+            while (numberPeople != 0)
             {
                 long min = windows[minIndex].Line.Count;
                 for (int i = 3; i >= 0; i--)
@@ -48,16 +27,120 @@ namespace LearnCS
                         minIndex = i;
                     }
                 }
-                Console.WriteLine(minIndex);
                 windows[minIndex].Line.Enqueue(1);
-                numberOfPeople -= 1;
+                numberPeople -= 1;
 
             }
-
             for (int i = 0; i < 4; i++)
             {
                 Console.WriteLine($"Counter {i + 1}: {windows[i].Line.Count} people");
             }
+
+        }
+
+        public static async Task removePeople(List<Window> windows)
+        {
+            await Task.Delay(1000);
+            Boolean isRunning = true;
+            while (isRunning)
+            {
+                isRunning = false;
+                for (int i = 0; i < windows.Count; i++)
+
+                {
+                    if (windows[i].Line.Count != 0)
+                    {
+                        if (windows[i].Line.Count == 1)
+                        {
+                            windows[i].Line.Dequeue();
+                            isRunning = true;
+                        }
+                        else
+                        {
+                            windows[i].Line.Dequeue();
+                            windows[i].Line.Dequeue();
+                            isRunning = true;
+                        }
+                    }
+                }
+                for (int i = 0; i < 4; i++)
+                {
+                    Console.WriteLine($"Number of People Waiting in window {i + 1} is {windows[i].Line.Count}");
+                }
+            }
+                
+            
+        }
+
+        public static async Task Main(String[] args)
+
+        {
+            
+            Window w1 = new Window();
+            Window w2 = new Window();
+            Window w3 = new Window();
+            Window w4 = new Window();
+
+            List<Window> windows = new List<Window>();
+            windows.Add(w1);
+            windows.Add(w2);
+            windows.Add(w3);
+            windows.Add(w4);
+
+            int[] numberPeople = new int[5] { 13,25,10,20,34 };
+
+            int minIndex = 0;
+            while (numberPeople[0] != 0)
+            {
+                long min = windows[minIndex].Line.Count;
+                for (int i = 3; i >= 0; i--)
+                {
+                    if (windows[i].Line.Count <= min)
+                    {
+                        min = windows[i].Line.Count;
+                        minIndex = i;
+                    }
+                }
+                windows[minIndex].Line.Enqueue(1);
+                numberPeople[0] -= 1;
+
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine($"Counter {i + 1}: {windows[i].Line.Count} people");
+            }
+
+            var removePeopleTask = removePeople(windows);
+
+            for (int i=1;i< numberPeople.Length; i++)
+            {
+                await Task.Delay(1000);
+                Simualte(numberPeople[i], windows);
+            }
+            //Console.WriteLine("Enter the number of people:");
+            //long numberOfPeople = long.Parse(Console.ReadLine());
+
+
+            //int minIndex = 0;
+            //while (numberOfPeople != 0)
+            //{
+            //    long min = windows[minIndex].Line.Count;
+            //    for (int i = 3; i >= 0; i--)
+            //    {
+            //        if (windows[i].Line.Count <= min)
+            //        {
+            //            min = windows[i].Line.Count;
+            //            minIndex = i;
+            //        }
+            //    }
+            //    windows[minIndex].Line.Enqueue(1);
+            //    numberOfPeople -= 1;
+
+            //}
+            //for (int i = 0; i < 4; i++)
+            //{
+            //    Console.WriteLine($"Counter {i + 1}: {windows[i].Line.Count} people");
+            //}
 
         }
     }
