81dce6b7cf10c466e861ae318b17c25204d777b6
introvertkunal <kunal.singh@wonderbiz.in>
2025-07-30 15:56:01 +0530

    First Logic

--- LearnCS/Program.cs
+++ LearnCS/Program.cs
@@ -10,7 +10,6 @@ namespace LearnCS
     class Window
     {
         public Queue<int> Line=new Queue<int>();
-        public int counter;
     }
     public class TicketCounter
     {
@@ -24,12 +23,7 @@ namespace LearnCS
             Window w3 = new Window();
             Window w4 = new Window();
 
-            Console.WriteLine($"{w1.Line.Count}");
-            Console.WriteLine($"{w2.Line.Count}");
-            Console.WriteLine($"{w3.Line.Count}");
-            Console.WriteLine($"{w4.Line.Count}");
-
-            int[] counter = new int[4];
+            //int[] counter = new int[4];
 
             List<Window> windows = new List<Window>();
             windows.Add(w1);
@@ -38,46 +32,32 @@ namespace LearnCS
             windows.Add(w4);
 
 
-            //Console.WriteLine("Enter the number of people:");
-            //long numberOfPeople = long.Parse(Console.ReadLine());
-            //Window minCounter;
-            //while (numberOfPeople != 0)
-            //{
-            //    long min = w1.counter;
-            //    foreach(var window in windows)
-            //    {
-            //        if (window!=w1 && window.counter <= min){
-            //            min = window.counter;
-            //            minCounter = window;
-            //        }
-            //    }
-
-            //    numberOfPeople -= 1;
-            //}
+            Console.WriteLine("Enter the number of people:");
+            long numberOfPeople = long.Parse(Console.ReadLine());
 
 
-            //int minIndex = 0;
-            //while (numberOfPeople != 0)
-            //{
-            //    long min = windows.w1.counter ;
-            //    for(int i = 3; i >=0; i--)
-            //    {
-            //        if (counter[i] <= min)
-            //        {
-            //            min = counter[i]; 
-            //            minIndex = i;
-            //        }
-            //    }
-            //    Console.WriteLine(minIndex);
-            //    counter[minIndex] += 1;
-            //    numberOfPeople -= 1;
+            int minIndex = 0;
+            while (numberOfPeople != 0)
+            {
+                long min = windows[minIndex].Line.Count;
+                for (int i = 3; i >= 0; i--)
+                {
+                    if (windows[i].Line.Count <= min)
+                    {
+                        min = windows[i].Line.Count;
+                        minIndex = i;
+                    }
+                }
+                Console.WriteLine(minIndex);
+                windows[minIndex].Line.Enqueue(1);
+                numberOfPeople -= 1;
 
-            //}
+            }
 
-            //for(int i = 0; i < 4; i++)
-            //{
-            //    Console.WriteLine($"Counter {i + 1}: {counter[i]} people");
-            //}
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine($"Counter {i + 1}: {windows[i].Line.Count} people");
+            }
 
         }
     }
