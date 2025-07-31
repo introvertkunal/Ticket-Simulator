7a93c8d4025cc3d9e014a01835b425de66f7ce68
introvertkunal <kunal.singh@wonderbiz.in>
2025-07-31 11:14:04 +0530

    "Final"

--- LearnCS/Program.cs
+++ LearnCS/Program.cs
@@ -1,19 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
 
-
-namespace LearnCS
-{
-
+namespace TicketSimulator
+{   
     public class Window
     {
-        public Queue<int> Line=new Queue<int>();
+        public Queue<int> Line = new Queue<int>();
     }
     public class TicketCounter
     {
-        public static void Simualte(int numberPeople,List<Window> windows)
+        public static void Stimulate(int numberPeople, List<Window> windows)
         {
             int minIndex = 0;
             while (numberPeople != 0)
@@ -29,126 +27,66 @@ namespace LearnCS
                 }
                 windows[minIndex].Line.Enqueue(1);
                 numberPeople -= 1;
-
             }
+            Console.WriteLine($"\n-------People Distributed among queue--------");
             for (int i = 0; i < 4; i++)
             {
                 Console.WriteLine($"Counter {i + 1}: {windows[i].Line.Count} people");
             }
-
         }
 
-        public static async Task removePeople(List<Window> windows)
+        public static void removePeople(List<Window> windows)
         {
-            await Task.Delay(1000);
-            Boolean isRunning = true;
-            while (isRunning)
+            for (int i = 0; i < 4; i++)
             {
-                isRunning = false;
-                for (int i = 0; i < windows.Count; i++)
-
-                {
-                    if (windows[i].Line.Count != 0)
-                    {
-                        if (windows[i].Line.Count == 1)
-                        {
-                            windows[i].Line.Dequeue();
-                            isRunning = true;
-                        }
-                        else
-                        {
-                            windows[i].Line.Dequeue();
-                            windows[i].Line.Dequeue();
-                            isRunning = true;
-                        }
-                    }
-                }
-                for (int i = 0; i < 4; i++)
-                {
-                    Console.WriteLine($"Number of People Waiting in window {i + 1} is {windows[i].Line.Count}");
-                }
+                if (windows[i].Line.Count > 0)
+                    windows[i].Line.Dequeue();
+                if (windows[i].Line.Count > 0)
+                    windows[i].Line.Dequeue();
             }
-                
-            
+        }
+        public static void printPeople(List<Window> windows, int minute)
+        {
+            Console.WriteLine($"\n----------Minute {minute}-------------");
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine($"The Number of People waiting in counter {i + 1} are: {windows[i].Line.Count}");
+                double waitingTime = ((windows[i].Line.Count + 1) * 30.0) / 60.0;
+                Console.WriteLine($"The Last Person is waiting in Counter {i + 1} about {waitingTime} Minute");
+            }
         }
 
-        public static async Task Main(String[] args)
-
+        public static async Task Main(string[] args)
         {
-            
             Window w1 = new Window();
             Window w2 = new Window();
             Window w3 = new Window();
             Window w4 = new Window();
 
-            List<Window> windows = new List<Window>();
-            windows.Add(w1);
-            windows.Add(w2);
-            windows.Add(w3);
-            windows.Add(w4);
+            List<Window> windows = new List<Window> { w1, w2, w3, w4 };
 
-            int[] numberPeople = new int[5] { 13,25,10,20,34 };
-
-            int minIndex = 0;
-            while (numberPeople[0] != 0)
+            int minute = 0;
+            string filePath = "C:\\Users\\Kunal Singh\\Desktop\\numberofpeople.txt.txt";
+            using (StreamReader sr = new StreamReader(filePath))
             {
-                long min = windows[minIndex].Line.Count;
-                for (int i = 3; i >= 0; i--)
+                string line;
+                while ((line = await sr.ReadLineAsync()) != null)
                 {
-                    if (windows[i].Line.Count <= min)
+                    if (int.TryParse(line, out int numberPeople))
                     {
-                        min = windows[i].Line.Count;
-                        minIndex = i;
+                        Stimulate(numberPeople, windows);
+                        removePeople(windows);
+                        printPeople(windows, ++minute);
+                        await Task.Delay(1000);
                     }
                 }
-                windows[minIndex].Line.Enqueue(1);
-                numberPeople[0] -= 1;
-
             }
-            for (int i = 0; i < 4; i++)
+            while (w1.Line.Count > 0 || w2.Line.Count > 0 || w3.Line.Count > 0 || w4.Line.Count > 0)
             {
-                Console.WriteLine($"Counter {i + 1}: {windows[i].Line.Count} people");
-            }
-
-            var removePeopleTask = removePeople(windows);
-
-            for (int i=1;i< numberPeople.Length; i++)
-            {
+                removePeople(windows);
+                printPeople(windows, ++minute);
                 await Task.Delay(1000);
-                Simualte(numberPeople[i], windows);
             }
-            //Console.WriteLine("Enter the number of people:");
-            //long numberOfPeople = long.Parse(Console.ReadLine());
-
-
-            //int minIndex = 0;
-            //while (numberOfPeople != 0)
-            //{
-            //    long min = windows[minIndex].Line.Count;
-            //    for (int i = 3; i >= 0; i--)
-            //    {
-            //        if (windows[i].Line.Count <= min)
-            //        {
-            //            min = windows[i].Line.Count;
-            //            minIndex = i;
-            //        }
-            //    }
-            //    windows[minIndex].Line.Enqueue(1);
-            //    numberOfPeople -= 1;
-
-            //}
-            //for (int i = 0; i < 4; i++)
-            //{
-            //    Console.WriteLine($"Counter {i + 1}: {windows[i].Line.Count} people");
-            //}
-
         }
     }
 }
-
-
-
-
-
-
-
